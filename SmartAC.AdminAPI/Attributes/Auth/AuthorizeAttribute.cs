dcc62bf40ee76af265b835916736c6eabb13556d
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-16 15:27:39 +0200

    adding custom Authorize Attribute

---
+++ SmartAC.AdminAPI/Attributes/Auth/AuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace SmartAC.AdminAPI.Attributes.Auth
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}

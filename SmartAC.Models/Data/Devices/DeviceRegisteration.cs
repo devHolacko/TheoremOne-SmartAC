84b34f13d59280e3f4278e6b83a03d346827dd34
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 12:43:33 +0200

    adding DeviceRegisteration entity

---
+++ SmartAC.Models/Data/Devices/DeviceRegisteration.cs
@@ -0,0 +1,15 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Devices
+{
+    public class DeviceRegisteration : BaseAuditedEntity
+    {
+        public Guid DeviceId { get; set; }
+        public string FirmwareVersion { get; set; }
+    }
+}

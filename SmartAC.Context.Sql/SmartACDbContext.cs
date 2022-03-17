9b771621cc325acb97a3eb67445451a2147ec2e8
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 16:25:47 +0200

    initializing DbContext project

---
+++ SmartAC.Context.Sql/SmartACDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SmartAC.Context.Sql
+{
+    public class SmartACDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("ConnectionString");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}

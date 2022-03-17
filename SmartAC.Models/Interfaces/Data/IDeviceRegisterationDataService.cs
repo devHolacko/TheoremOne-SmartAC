fe57b189eaef32c3553e5cdb8cbde3dd4b7da390
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 21:57:02 +0200

    adding DeviceRegisterationDataService interface

---
+++ SmartAC.Models/Interfaces/Data/IDeviceRegisterationDataService.cs
@@ -0,0 +1,18 @@
+﻿using SmartAC.Models.Data.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Interfaces.Data
+{
+    public interface IDeviceRegisterationDataService
+    {
+        public void CreateDevice(DeviceRegisteration device);
+        public void EditDevice(DeviceRegisteration device);
+        public DeviceRegisteration GetDeviceById(Guid id);
+        public IEnumerable<DeviceRegisteration> GetDevices(Expression<Func<Device, bool>> expression);
+    }
+}

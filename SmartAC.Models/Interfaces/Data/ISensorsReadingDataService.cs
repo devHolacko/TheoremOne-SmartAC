6f85a63db4f6d7ca69fe69f6e04a07a292c58559
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 22:01:32 +0200

    adding SensorsReadingDataService interface

---
+++ SmartAC.Models/Interfaces/Data/ISensorsReadingDataService.cs
@@ -0,0 +1,18 @@
+﻿using SmartAC.Models.Data.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Interfaces.Data
+{
+    public interface ISensorsReadingDataService
+    {
+        public void CreateDevice(SensorsReading device);
+        public void EditDevice(SensorsReading device);
+        public SensorsReading GetDeviceById(Guid id);
+        public IEnumerable<SensorsReading> GetDevices(Expression<Func<SensorsReading, bool>> expression);
+    }
+}

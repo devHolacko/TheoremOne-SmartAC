ce8bcbe587bac6076e3c13221d729b8098b4adf7
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 22:04:53 +0200

    adding InvalidSensorsReadingDataService interfacE

---
+++ SmartAC.Models/Interfaces/Data/IInvalidSensorsReadingDataService.cs
@@ -0,0 +1,18 @@
+﻿using SmartAC.Models.Data.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Interfaces.Data
+{
+    public interface IInvalidSensorsReadingDataService
+    {
+        public void CreateDevice(InvalidSensorReading device);
+        public void EditDevice(InvalidSensorReading device);
+        public InvalidSensorReading GetDeviceById(Guid id);
+        public IEnumerable<InvalidSensorReading> GetDevices(Expression<Func<InvalidSensorReading, bool>> expression);
+    }
+}

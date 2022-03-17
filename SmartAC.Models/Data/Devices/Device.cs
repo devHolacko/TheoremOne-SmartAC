177f8abefd7178a74cfc8c5488837d1244c70c79
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 12:42:13 +0200

    adding Device entity

---
+++ SmartAC.Models/Data/Devices/Device.cs
@@ -0,0 +1,15 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Devices
+{
+    public class Device : BaseAuditedEntity
+    {
+        public string Serial { get; set; }
+        public string Secret { get; set; }
+    }
+}

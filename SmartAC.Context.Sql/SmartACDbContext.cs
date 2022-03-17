437b3e3e35bb4e5e78966a7fce7195acb569dbc8
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 20:14:10 +0200

    configuring entities using fluent api

--- SmartAC.Context.Sql/SmartACDbContext.cs
+++ SmartAC.Context.Sql/SmartACDbContext.cs
@@ -29,6 +29,82 @@ namespace SmartAC.Context.Sql
                     }
                 }
             }
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Active).HasDefaultValue(true);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasDefaultValueSql("NEWID()");
+                entity.Property(e => e.CreatedOn).ValueGeneratedOnAdd().HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.ModifiedOn).ValueGeneratedOnUpdate().HasDefaultValueSql("GETUTCDATE()");
+
+                entity.Property(e => e.Serial).IsRequired();
+                entity.Property(e => e.Secret).IsRequired();
+            });
+
+            modelBuilder.Entity<Alert>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Active).HasDefaultValue(true);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasDefaultValueSql("NEWID()");
+                entity.Property(e => e.CreatedOn).ValueGeneratedOnAdd().HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.ModifiedOn).ValueGeneratedOnUpdate().HasDefaultValueSql("GETUTCDATE()");
+
+                entity.Property(e => e.DeviceId).IsRequired();
+                entity.Property(e => e.Message).IsRequired();
+                entity.Property(e => e.Type).IsRequired();
+                entity.Property(e => e.Code).IsRequired();
+                entity.Property(e => e.AlertDate).IsRequired();
+                entity.Property(e => e.ViewStatus).IsRequired();
+                entity.Property(e => e.ResolutionStatus).IsRequired();
+
+                entity.HasOne(c => c.Device).WithMany(c => c.Alerts).HasForeignKey(c => c.DeviceId);
+            });
+
+            modelBuilder.Entity<DeviceRegisteration>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Active).HasDefaultValue(true);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasDefaultValueSql("NEWID()");
+                entity.Property(e => e.CreatedOn).ValueGeneratedOnAdd().HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.ModifiedOn).ValueGeneratedOnUpdate().HasDefaultValueSql("GETUTCDATE()");
+
+                entity.Property(e => e.DeviceId).IsRequired();
+                entity.Property(e => e.FirmwareVersion).IsRequired();
+
+                entity.HasOne(c => c.Device).WithMany(c => c.DeviceRegisterations).HasForeignKey(c => c.DeviceId);
+            });
+
+            modelBuilder.Entity<SensorsReading>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Active).HasDefaultValue(true);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasDefaultValueSql("NEWID()");
+                entity.Property(e => e.CreatedOn).ValueGeneratedOnAdd().HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.ModifiedOn).ValueGeneratedOnUpdate().HasDefaultValueSql("GETUTCDATE()");
+
+                entity.Property(e => e.DeviceId).IsRequired();
+                entity.Property(e => e.Temperature).IsRequired();
+                entity.Property(e => e.Humidity).IsRequired();
+                entity.Property(e => e.CarbonMonoxide).IsRequired();
+                entity.Property(e => e.HealthStatus).IsRequired();
+
+                entity.HasOne(c => c.Device).WithMany(c => c.DeviceSensorReadings).HasForeignKey(c => c.DeviceId);
+            });
+
+            modelBuilder.Entity<InvalidSensorReading>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Active).HasDefaultValue(true);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd().HasDefaultValueSql("NEWID()");
+                entity.Property(e => e.CreatedOn).ValueGeneratedOnAdd().HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.ModifiedOn).ValueGeneratedOnUpdate().HasDefaultValueSql("GETUTCDATE()");
+
+                entity.Property(e => e.DeviceId).IsRequired();
+                entity.Property(e => e.Data).IsRequired().HasMaxLength(500);
+
+                entity.HasOne(c => c.Device).WithMany(c => c.DeviceInvalidReadings).HasForeignKey(c => c.DeviceId);
+            });
         }
 
         public DbSet<Device> Devices { get; set; }

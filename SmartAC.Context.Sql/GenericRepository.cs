2a5abdddf78e7c4bf66d5bc8b8861289a3a75b4e
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 19:25:16 +0200

    implementing GenericRepository

---
+++ SmartAC.Context.Sql/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartAC.Models.Data.Base;
+using SmartAC.Models.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Context.Sql
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+        where T : BaseEntity
+    {
+        private readonly SmartACDbContext _context;
+        private readonly DbSet<T> _entities;
+        public GenericRepository(SmartACDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+        public T Get(Guid id)
+        {
+            return _entities.SingleOrDefault(s => s.Id == id);
+        }
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Add(entity);
+            _context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _context.SaveChanges();
+        }
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}

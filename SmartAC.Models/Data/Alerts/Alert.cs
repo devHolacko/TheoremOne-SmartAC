7bed05f394655891efff6ae5ed14654254d66588
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 12:51:02 +0200

    Adding Alert entity + enum basis

---
+++ SmartAC.Models/Data/Alerts/Alert.cs
@@ -0,0 +1,21 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Alerts
+{
+    public class Alert : BaseAuditedEntity
+    {
+        public Guid DeviceId { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public string Code { get; set; }
+        public DateTime AlertDate { get; set; }
+        public DateTime? ResolutionDate { get; set; }
+        public string ViewStatus { get; set; }
+        public string ResolutionStatus { get; set; }
+    }
+}

3058139e882da28a9ac5cd5c6bfafba501d484f4
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 13:27:50 +0200

    Adding IGenericRepository interface

---
+++ SmartAC.Models/Interfaces/Common/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Interfaces.Common
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}

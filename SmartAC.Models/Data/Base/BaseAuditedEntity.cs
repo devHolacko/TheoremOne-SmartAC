e3cd3d34cf639d2c2c34fd20bd0c8d23bffe6a82
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 12:37:40 +0200

    adding BaseAuditedEntity class

---
+++ SmartAC.Models/Data/Base/BaseAuditedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Base
+{
+    public abstract class BaseAuditedEntity : BaseEntity
+    {
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}

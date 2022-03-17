c5358a042738d2e4f35d10622f87df8220a21b71
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 12:45:33 +0200

    adding SensorsReading entity

---
+++ SmartAC.Models/Data/Sensors/SensorsReading.cs
@@ -0,0 +1,17 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Sensors
+{
+    public class SensorsReading : BaseAuditedEntity
+    {
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public decimal CarbonMonoxide { get; set; }
+        public string HealthStatus { get; set; }
+    }
+}

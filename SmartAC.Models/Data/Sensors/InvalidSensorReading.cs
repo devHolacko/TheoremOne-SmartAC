06e1f38a835a650e6a182a110e3b1f91f08ba83a
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-17 12:55:03 +0200

    adding InvalidSensorReading entity

---
+++ SmartAC.Models/Data/Sensors/InvalidSensorReading.cs
@@ -0,0 +1,15 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Sensors
+{
+    public class InvalidSensorReading : BaseAuditedEntity
+    {
+        public Guid DeviceId { get; set; }
+        public string Data { get; set; }
+    }
+}

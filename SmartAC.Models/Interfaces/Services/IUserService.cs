85e41d69871c4a15506fb4a1eba932e0a3cdcab4
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 20:21:17 +0200

    adding UserService interface

---
+++ SmartAC.Models/Interfaces/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using SmartAC.Models.ViewModels.Requests.Users;
+using SmartAC.Models.ViewModels.Responses.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Interfaces.Services
+{
+    public interface IUserService
+    {
+        DataGenericResponse<string> Login(LoginRequest request);
+    }
+}

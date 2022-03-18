20bec9b21c6ecd3e04c03c8328d1de0a150dca68
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 20:13:46 +0200

    adding LoginRequest model

---
+++ SmartAC.Models/ViewModels/Requests/Users/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.ViewModels.Requests.Users
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}

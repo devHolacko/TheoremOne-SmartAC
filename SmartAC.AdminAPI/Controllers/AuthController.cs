508ea2c19277c169f02ac726a4027ac3779a93ea
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 21:59:36 +0200

    adding AuthController with Login and Logout

---
+++ SmartAC.AdminAPI/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartAC.AdminAPI.Controllers.Base;
+using SmartAC.Models.Interfaces.Services;
+using SmartAC.Models.ViewModels.Requests.Users;
+using SmartAC.Models.ViewModels.Responses.Base;
+
+namespace SmartAC.AdminAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : BaseAuthController
+    {
+        private readonly IUserService _userService;
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// An api that authenticates the user if the credentials are correct. Generates JWT token
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            DataGenericResponse<string> response = _userService.Login(request);
+            return new OkObjectResult(response);
+        }
+
+        /// <summary>
+        /// An api that deletes the user session. Any generated tokens will be invalid
+        /// </summary>
+        /// <returns></returns>
+        [Route("logout")]
+        [HttpDelete]
+        public IActionResult Logout()
+        {
+            GenericResponse response = _userService.Logout();
+            return new OkObjectResult(response);
+        }
+    }
+}
---
+++ SmartAC.AdminAPI/Controllers/Base/BaseAuthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartAC.AdminAPI.Controllers.Base
+{
+    public class BaseAuthController : ControllerBase
+    {
+    }
+}

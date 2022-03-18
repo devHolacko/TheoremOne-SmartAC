044b6317fbd31d1ff6035e27559967efcbb76bc9
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 20:19:42 +0200

    Registering LoginValidator

--- SmartAC.Services/ServicesStartup.cs
+++ SmartAC.Services/ServicesStartup.cs
@@ -5,8 +5,10 @@ using SmartAC.Models.Interfaces.Services;
 using SmartAC.Models.Validations.Alerts;
 using SmartAC.Models.Validations.DeviceRegisterations;
 using SmartAC.Models.Validations.Devices;
+using SmartAC.Models.Validations.Users;
 using SmartAC.Models.ViewModels.Requests.Alerts;
 using SmartAC.Models.ViewModels.Requests.Devices;
+using SmartAC.Models.ViewModels.Requests.Users;
 using SmartAC.Services.Alerts;
 using SmartAC.Services.Devices;
 
@@ -24,6 +26,7 @@ namespace SmartAC.Services
             services.AddTransient<IValidator<RegisterDeviceRequest>, RegisterDeviceValidator>();
             services.AddTransient<IValidator<ReportDeviceReadingsRequest>, ReportDeviceReadingsValidator>();
             services.AddTransient<IValidator<CreateAlertRequest>, CreateAlertValidator>();
+            services.AddTransient<IValidator<LoginRequest>, LoginValidator>();
         }
     }
 }

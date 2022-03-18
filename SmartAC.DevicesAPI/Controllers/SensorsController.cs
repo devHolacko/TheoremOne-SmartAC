93b07fede6d236903a620119f04510d708437b1a
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-19 00:24:52 +0200

    Adding api to get sensor readings of a device give date range

--- SmartAC.DevicesAPI/Controllers/SensorsController.cs
+++ SmartAC.DevicesAPI/Controllers/SensorsController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SmartAC.DevicesAPI.Controllers.Base;
+using SmartAC.Models.Interfaces.Services;
+using SmartAC.Models.ViewModels.Responses.Base;
+using SmartAC.Models.ViewModels.Responses.Sesnors;
+using System;
+using System.Collections.Generic;
 
 namespace SmartAC.DevicesAPI.Controllers
 {
@@ -8,10 +13,19 @@ namespace SmartAC.DevicesAPI.Controllers
     [ApiController]
     public class SensorsController : BaseAuthController
     {
-        
-        public SensorsController()
+        private readonly ISensorsReadingService _sensorsReadingService;
+        public SensorsController(ISensorsReadingService sensorsReadingService)
         {
+            _sensorsReadingService = sensorsReadingService;
+        }
 
+        [Route("devices/{device-id}/readings/size/{page-size}/number/{page-number}")]
+        [HttpGet]
+        public IActionResult GetDeviceSensorReadingsByDateRange([FromRoute(Name = "device-id")] Guid deviceId, [FromRoute(Name = "page-size")] int pageSize, [FromRoute(Name = "page-number")] int pageNumber,
+            [FromQuery(Name = "from")] DateTime? fromDate, [FromQuery(Name = "to")] DateTime? toDate)
+        {
+            DataGenericResponse<List<SensorReadingsResponseViewModel>> response = _sensorsReadingService.GetSensorReadings(deviceId, fromDate, toDate, pageNumber, pageSize);
+            return new OkObjectResult(response);
         }
     }
 }

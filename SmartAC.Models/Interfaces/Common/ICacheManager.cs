647c7cc7aa767611bbaf8a5ceb130457e0da4e87
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 21:17:15 +0200

    adding CacheManager interface

---
+++ SmartAC.Models/Interfaces/Common/ICacheManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Interfaces.Common
+{
+    public interface ICacheManager
+    {
+        void Add(string key, string value);
+        string Get(string key);
+    }
+}

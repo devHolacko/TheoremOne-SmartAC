8f9a29eaf56aebbc363478a498e3c271c4c8fd66
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 11:39:41 +0200

    Adding generic response models

---
+++ SmartAC.Models/ViewModels/Responses/Base/DataGenericResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.ViewModels.Responses.Base
+{
+    public class DataGenericResponse<T> : GenericResponse
+        where T : class, new()
+    {
+        public T Data { get; set; }
+    }
+}

3cd8245f1f1d9dfe1be0278d4a4596ee85272581
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 22:01:16 +0200

    Adding Devices controller

---
+++ SmartAC.DevicesAPI/Controllers/DevicesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartAC.DevicesAPI.Controllers.Base;
+using SmartAC.Models.Interfaces.Services;
+
+namespace SmartAC.DevicesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : BaseAuthController
+    {
+        private readonly IDeviceService _deviceService;
+        public DevicesController(IDeviceService deviceService)
+        {
+            _deviceService = deviceService;
+        }
+    }
+}

06a1a62b26c3d08f7caab607cd7465a2dc2dc151
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 11:50:44 +0200

    adding RegisterDeviceRequest model

---
+++ SmartAC.Models/ViewModels/Requests/Devices/RegisterDeviceRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.ViewModels.Requests.Devices
+{
+    public class RegisterDeviceRequest
+    {
+        public string Serial { get; set; }
+        public string Secret { get; set; }
+    }
+}

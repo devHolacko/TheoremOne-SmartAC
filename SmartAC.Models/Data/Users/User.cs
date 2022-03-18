5cdff4e8abd18d5caf38898fe09c159ddfd7af07
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 20:12:25 +0200

    adding User entity

---
+++ SmartAC.Models/Data/Users/User.cs
@@ -0,0 +1,18 @@
+﻿using SmartAC.Models.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Models.Data.Users
+{
+    public class User : BaseAuditedEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}

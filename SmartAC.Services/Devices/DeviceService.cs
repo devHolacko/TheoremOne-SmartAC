508326c68a8f5c56f0168839f87a3efc846141aa
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-18 13:21:06 +0200

    adding DeviceService

---
+++ SmartAC.Services/Devices/DeviceService.cs
@@ -0,0 +1,26 @@
+﻿using SmartAC.Models.Interfaces.Data;
+using SmartAC.Models.Interfaces.Services;
+using SmartAC.Models.ViewModels.Requests.Devices;
+using SmartAC.Models.ViewModels.Responses.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAC.Services.Devices
+{
+    public class DeviceService : IDeviceService
+    {
+        private readonly IDeviceDataService _deviceService;
+        public DeviceService(IDeviceDataService deviceDataService)
+        {
+            _deviceService = deviceDataService;
+        }
+
+        public GenericResponse Register(RegisterDeviceRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

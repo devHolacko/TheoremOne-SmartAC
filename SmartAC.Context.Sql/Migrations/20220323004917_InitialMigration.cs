6c3dfab7fa24e394e87e7a09e3e6807ebd21212b
Abdullah El-Menawy <a.elmenawy@outlook.com>
2022-03-23 02:50:51 +0200

    adding initial migration after modifications

---
+++ SmartAC.Context.Sql/Migrations/20220323004917_InitialMigration.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartAC.Context.Sql.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Serial = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Secret = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeviceRegisterations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirmwareVersion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceRegisterations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceRegisterations_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvalidSensorReadings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvalidSensorReadings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InvalidSensorReadings_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorReadings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Temperature = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Humidity = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CarbonMonoxide = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    HealthStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecordedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorReadings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SensorReadings_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SensorReadingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AlertDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ResolutionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ViewStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ResolutionStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alerts_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Alerts_SensorReadings_SensorReadingId",
+                        column: x => x.SensorReadingId,
+                        principalTable: "SensorReadings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Devices",
+                columns: new[] { "Id", "ModifiedOn", "Secret", "Serial" },
+                values: new object[,]
+                {
+                    { new Guid("7997cfa3-99b2-40f9-ae97-18efdd6b348d"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "p19fym3p3u4dhmw2e6r1m34ulj0bsf83", "HEP10sO7XlOVxFHH3N6CeoOcYzp1H" },
+                    { new Guid("ef2930a0-40e8-4f19-9ad1-cbd9ac639c27"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "f92o5utv1xj4qo6wn990cxcuz6jizekx", "3aW93UxRg1nPvKsLGICwc73PX3pLL43s" },
+                    { new Guid("c7b64f64-59ac-47b9-8d97-e1d21cec4406"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "iv10ytx4ax397beocgpvghu0txr2yo5r", "tmKj7i6wTGO4MZ9WteG0AEw8z0raXn" },
+                    { new Guid("681378c9-48c1-44d6-a4dd-00a498073cc7"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "41511oh5lqerycl8g5u0db7vy4qh4jge", "R7v5qusR5QLYaZw7AMcb3W5tNkTuUgE" },
+                    { new Guid("ba35fd57-1831-43b7-99b7-9dbd912fbde2"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "u54wpi8xphnn6p50yt3vbkfadnkcn9ld", "iO5FKAwUYW3m5jew7KTDMe7JuCnuBq" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_DeviceId",
+                table: "Alerts",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_SensorReadingId",
+                table: "Alerts",
+                column: "SensorReadingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceRegisterations_DeviceId",
+                table: "DeviceRegisterations",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvalidSensorReadings_DeviceId",
+                table: "InvalidSensorReadings",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorReadings_DeviceId",
+                table: "SensorReadings",
+                column: "DeviceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alerts");
+
+            migrationBuilder.DropTable(
+                name: "DeviceRegisterations");
+
+            migrationBuilder.DropTable(
+                name: "InvalidSensorReadings");
+
+            migrationBuilder.DropTable(
+                name: "SensorReadings");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
